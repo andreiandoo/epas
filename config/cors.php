52a56f824253e81398c3053dc19135ce430cc04a
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2025-11-22 23:15:13 +0200

    fix: Allow all origins in Laravel CORS config

--- config/cors.php
+++ config/cors.php
@@ -2,15 +2,10 @@
 return [
   'paths' => ['api/*', 'sanctum/csrf-cookie'],
   'allowed_methods' => ['*'],
-  'allowed_origins' => [
-    'http://odeon.local:8082',
-    'http://embed.local:8083',
-    'http://localhost:8082',
-    'http://localhost:8083',
-  ],
+  'allowed_origins' => ['*'],
   'allowed_origins_patterns' => [],
   'allowed_headers' => ['*'],
   'exposed_headers' => [],
-  'max_age' => 0,
-  'supports_credentials' => false,
+  'max_age' => 86400,
+  'supports_credentials' => true,
 ];
