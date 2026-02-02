f42b670bb85fc7a94a154288e6354f169819abce
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-02-02 18:08:04 +0200

    Deploy marketplace 2026-02-02 18:08

--- organizer/events.php
+++ organizer/events.php
@@ -752,7 +752,7 @@ function renderEvents(events) {
                             </div>
                         </div>
                         <div class="flex flex-wrap items-center justify-end gap-2 pr-4">
-                            <a href="/organizator/event/${event.id}?action=edit" class="btn btn-sm btn-secondary" title="Editează"><svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg></a>
+                            ${event.is_editable !== false ? `<a href="/organizator/event/${event.id}?action=edit" class="btn btn-sm btn-secondary" title="Editează"><svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg></a>` : ''}
                             ${documentsButton}
                             ${analyticsButton}
                             ${promoteButton}
