49cf0ad89db92def0327da8d2c43fb68cf5939f4
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-02-24 13:03:13 +0200

    Stuff added

--- assets/js/pages/event-single.js
+++ assets/js/pages/event-single.js
@@ -1889,7 +1889,7 @@ const EventPage = {
             html += '<div class="flex-1 min-w-0">';
             if (location) {
                 html += '<p class="font-semibold text-secondary truncate group-hover:text-primary transition-colors">';
-                html += city ? esc(city) : '';
+                html += city ? city : '';
                 html += '<svg class="text-primary inline w-3.5 h-3.5 ml-1 mr-1 -mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>';
                 html += '<span class="text-primary">' + location + '</span></p>';
             }
