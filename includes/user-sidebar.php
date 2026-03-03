24e0591457bb5e771f673762950bf7a532439ac4
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-03-03 18:21:09 +0200

    Deploy marketplace 2026-03-03 18:20

--- includes/user-sidebar.php
+++ includes/user-sidebar.php
@@ -24,7 +24,7 @@ $currentPage = $currentPage ?? 'dashboard';
 ?>
 
 <!-- Mobile Menu Toggle -->
-<button id="mobile-sidebar-toggle" class="fixed z-40 justify-center text-white rounded-full flex items-center gap-x-2 shadow-lg lg:hidden bottom-4 right-0 left-0 mx-auto w-[90%] h-14 bg-primary">
+<button id="mobile-sidebar-toggle" class="mobile:hidden fixed z-40 justify-center text-white rounded-full items-center gap-x-2 shadow-lg lg:hidden bottom-4 right-0 left-0 mx-auto w-[90%] h-14 bg-primary">
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/></svg>
     <span class="">Meniul meu</span>
 </button>
--- includes/user-wrap-end.php
+++ includes/user-wrap-end.php
@@ -1,6 +1,11 @@
 <?php
 
 ?>
+            <!-- Mobile Menu Toggle -->
+            <button id="mobile-sidebar-toggle" class="hidden sticky z-40 justify-center text-white rounded-full mobile:flex items-center gap-x-2 shadow-lg lg:hidden bottom-4 right-0 left-0 mx-auto w-[90%] h-14 bg-primary">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/></svg>
+                <span class="">Meniul meu</span>
+            </button>
         </main>
     </div>
 </div>
